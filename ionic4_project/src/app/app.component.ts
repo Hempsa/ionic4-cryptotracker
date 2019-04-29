591edbbf95583b87aac3c7f9005609fcf75e1def
Henkka <henkkaah@windowslive.com>
2019-04-29 20:06:55 +0300

    Back- toiminnallisuus lisätty.

--- ionic4_project/src/app/app.component.ts
+++ ionic4_project/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, SystemJsNgModuleLoader } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -39,7 +39,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private navCtrl: NavController
   ) {
     this.initializeApp();
   }
@@ -48,6 +49,9 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.platform.backButton.subscribeWithPriority(999, () => {
+        navigator['app'].exitApp();
+      });
     });
   }
 }
